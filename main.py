7dca21a5d602e0dc59a32a016fa4d0e8119b0452
Tharun Vemula <tharunvind@gmail.com>
2024-03-28 12:14:32 +0530

    update codebase

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
-import face_recognition
 from flask import Flask, render_template, request
-from os.path import join, dirname, abspath
-import numpy as np
+import face_recognition
 import cv2
+import numpy as np
+import os
+import requests
 
-filename = join(dirname(__file__), "model", "predictor.pkl")
-current_dir = dirname(abspath(__file__))
-
-model = np.load(open(filename, "rb"))
-known_face_encodings = model['known_face_encodings']
-known_face_names = model['known_face_names']
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_word():
-    return {"ok" : "initialised"}
+@app.route("/", methods=["GET"])
+def hello_world():
+    return render_template("index.html")
 
 
 @app.route("/submit", methods=["POST"])
-def recognise_face():
+def face_recog():
+    data = request.json
+    imagefile = data.get("fileName")
+    userId = data.get("id").upper()
+    image_path = "../../photos/" + imagefile
 
-    photo_url = str(request.form['photoURL'])
-    file_path = join(current_dir, '..', 'backend', 'photos', photo_url)
-    frame = cv2.imread(file_path)
+    nodejs_server_url = "http://172.16.207.194:3000/api/v1/populate/face/recognition"
+
+    trained_model = np.load("trained_model.npz")
+    known_face_encodings = trained_model["known_face_encodings"]
+    known_face_names = trained_model["known_face_names"]
+
+    # Load the image
+    frame = cv2.imread(image_path)
+
+    # Resize frame of the image to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+
+    # Find all the faces and face encodings in the current frame of the image
     face_locations = face_recognition.face_locations(rgb_small_frame)
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    face_names = []
+
     for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         name = "Unknown"
+        is_matched = False
 
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(
+            known_face_encodings, face_encoding
+        )
         best_match_index = np.argmin(face_distances)
+
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
+            name = os.path.splitext(os.path.basename(name))[0]
+        if name == userId:
+            is_matched = True
 
-        face_names.append(name)
-    
-    if len(face_names) > 0:
-        return {"status" : 200, "identification": face_names}
-    return {"status" : 404, "identification" : []}
+    # return face_names
+    if is_matched:
+        response = {"status": 200, "identification": name}
+
+    else:
+        response = {"status": 404, "identification": []}
+
+    result = requests.post(nodejs_server_url, json=response)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5510, debug=True)
